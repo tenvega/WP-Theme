5cd66600af45754d2158e979b7bf2ebf5b232691
Enoc <etorra@gmail.com>
2017-10-18 13:58:39 -0400

    theme creation

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+function learningWordpress_resources(){
+
+  wp_enqueue_style('style', get_stylesheet_uri());
+
+}
+add_action('wp_enqueue_scripts', 'learningWordpress_resources');
+
+//navigation menus//
+register_nav_menus(array(
+  'primary' => __('Primary Menu'),
+  'footer' => __('Footer Menu'),
+));
