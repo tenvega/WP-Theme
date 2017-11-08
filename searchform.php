d612655a661fe054d291192ee1cc77053466e6c2
Enoc <etorra@gmail.com>
2017-11-08 16:37:16 -0500

    search logic added

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
 <form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 <div><label for="s" class="assistive-text"><?php _e( 'Search', 'twentyeleven' ); ?></label>
-  <input type="text" class="field" name="s" id="s" placeholder="<?php esc_attr_e( 'Search', 'twentyeleven' ); ?>" />
+  <input type="text" class="field" name="s" id="s" placeholder="<?php the_search_query(); ?>" />
   <input type="submit" class="submit" name="submit" id="searchsubmit" value="Search" />
 </div>
 </form>
