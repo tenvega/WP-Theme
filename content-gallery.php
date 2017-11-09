b6b621e426e0076d77ab2259418ff656a6fb8e15
Enoc <etorra@gmail.com>
2017-11-09 14:44:42 -0500

    added content-gallery

---
+++ content-gallery.php
@@ -0,0 +1,10 @@
+<article class="post post-gallery">
+
+  <article class="post post-gallery">
+
+  	<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+  	<?php the_content(); ?>
+
+  </article>
+
+</article>
